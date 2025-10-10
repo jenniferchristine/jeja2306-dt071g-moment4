4d7267c5c195cbdbdda8c8f9a6e6fd8b14a97385
your.user.name <jeja2306@student.miun.se>
2025-10-10 10:11:54 +0200

    changed console messages

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ namespace ReviewClassifier
             while (active) // snurrar loopen så länge varibeln är true
             {
                 Console.Clear();
-                Console.WriteLine("Please write a review:\n");
+                Console.WriteLine("Write a review:\n");
                 string? inputReview = Console.ReadLine();
 
                 if (!string.IsNullOrEmpty(inputReview)) // om giltig input angivits så skickar den vidare för bedömning
                 {
                     string prediction = ModelService.ClassifyReview(inputReview);
-                    Console.WriteLine($"\nYour review was deemed {prediction}!");
+                    Console.WriteLine($"\nYour review was deemed: {prediction}!");
                 }
                 else
                 {
