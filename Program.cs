67f4eb9e477eb3c851ee36edbfefaa107837b3e8
your.user.name <jeja2306@student.miun.se>
2025-10-10 09:48:57 +0200

    added comments

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace ReviewClassifier
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // huvudloop
         {
-            bool active = true;
+            bool active = true; // variabel för loopen
 
-            while (active)
+            while (active) // snurrar loopen så länge varibeln är true
             {
                 Console.Clear();
                 Console.WriteLine("Please write a review:\n");
                 string? inputReview = Console.ReadLine();
 
-                if (!string.IsNullOrEmpty(inputReview))
+                if (!string.IsNullOrEmpty(inputReview)) // om giltig input angivits så skickar den vidare för bedömning
                 {
                     string prediction = ModelService.ClassifyReview(inputReview);
                     Console.WriteLine($"\nYour review was deemed {prediction}!");
